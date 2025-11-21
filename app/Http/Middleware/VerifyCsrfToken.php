c1c6904341aa4eb7d5640a1f9494bf878c66289a
yaaeww <muhammadihya11289@gmail.com>
2025-11-21 12:39:08 +0700

    kiww

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -9,9 +9,12 @@ class VerifyCsrfToken extends Middleware
     /**
      * The URIs that should be excluded from CSRF verification.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $except = [
-        //
+        '/midtrans/callback',
+        'midtrans/callback',
+        'payment/*',
+        'api/*'
     ];
 }
